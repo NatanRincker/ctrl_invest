3206262745d95f5276c0e094ea78bcb8c020ac5e
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2025-08-14 00:38:22 +0000

    install `jest`| implement first test against `api/v1/status` endpoint

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,11 @@
+describe("GET to /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody).toBeDefined();
+    });
+  });
+});
