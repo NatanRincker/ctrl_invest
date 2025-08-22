59b65aefc42e6009f5d62ba02af9cfb7004a9f03
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2025-08-22 03:18:02 +0000

    add `migrations` tests

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,30 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+});
+
+describe("POST to /api/v1/migrations", () => {
+  describe("Anonymous User Running pending migrations", () => {
+    test("For the 1st time", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+      });
+      expect(response1.status).toBe(201);
+
+      const response1Body = await response1.json();
+
+      expect(Array.isArray(response1Body)).toBe(true);
+      expect(response1Body.length).toBeGreaterThan(0);
+    });
+    test("For the 2nd time", async () => {
+      const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+      });
+      expect(response2.status).toBe(200);
+      const response2Body = await response2.json();
+      expect(response2Body.length).toBe(0);
+    });
+  });
+});
