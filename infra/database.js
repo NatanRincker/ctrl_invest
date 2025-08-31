ba44d14a38fd9780060229a62942174be8aa0ee9
NatanRincker <46805756+NatanRincker@users.noreply.github.com>
2025-08-31 19:55:52 +0000

    add `controller.js` as abstraction | add `MethodNotAllowedError` and `ServiceError` to `errors.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Error inside database.js:");
-    console.log(error);
-    throw error;
+    const ServiceErrorObject = new ServiceError({
+      message: "error while performing connection or query",
+      cause: error,
+    });
+    throw ServiceErrorObject;
   } finally {
     await client?.end();
   }
